80efddef20493849ca3d670bf153c08572148ae1
Benjamin Deutsch <benjaminrdeutsch@gmail.com>
2026-03-28 16:13:46 -0400

    Bring logic.c assets into scope for main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "logic.c"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -94,7 +94,7 @@ int main(void)
   MX_GPIO_Init();
   MX_CAN_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start(&htim2);  // for DRS timer logic
   /* USER CODE END 2 */
 
   /* Infinite loop */
