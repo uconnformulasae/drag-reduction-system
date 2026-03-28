652b0e3cd566233c85016f5f036e3c3065990d61
Benjamin Deutsch <benjaminrdeutsch@gmail.com>
2026-03-28 16:13:11 -0400

    Better docs for logic.c

--- Core/Src/logic.c
+++ Core/Src/logic.c
@@ -1,6 +1,11 @@
 // logic.c
 // B. Deutsch (2/3/26)
 
+#include "main.h"
+
+/*
+ * CRITICAL PARAMETERS
+ */
 #define MAX_ABS_STEERING_ANGLE 30		// source: jake-ish
 #define MAX_ABS_LAT_G 0.35				// according to sample Michigan corner data
 #define MAX_BRAKE_INPUT_PERCENTAGE 0.2	// source: it came to me in a dream
@@ -8,11 +13,17 @@
 #define MODE 0
 //#define MAX_ABS_LONG_G 0
 
+/*
+ * ASST FUNCTIONS
+ */
 // Returns absolute value of float argument f
 float absFloat(float f) {
 	return f >= 0 ? f : -1 * f;
 }
 
+/*
+ * MAIN LOGIC
+ */
 // Returns whether DRS should be enabled given the input info
 bool shouldActivateDRS(float steeringAngle, float latG, float brakeInputPercentage, float* cooldown) {
 	return (
