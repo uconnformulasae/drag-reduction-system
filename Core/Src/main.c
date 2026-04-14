5570ba1ea497eaffb390eb5a905dcb736557839f
Benjamin Deutsch <benjaminrdeutsch@gmail.com>
2026-04-14 19:38:13 -0400

    Remove explicit file inclusions to allow linker to handle this itself; comment out unimplemented features

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "logic.c"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,9 +44,12 @@
 /* Private variables ---------------------------------------------------------*/
 CAN_HandleTypeDef hcan;
 
-MODE mode = AUTO;
-DRS_STATUS drs_stat = OFF;
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
+
+//MODE mode = AUTO;
+//DRS_STATUS drs_stat = OFF;
 
 /* USER CODE END PV */
 
@@ -54,6 +57,7 @@ DRS_STATUS drs_stat = OFF;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_CAN_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -93,47 +97,34 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_CAN_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim2);  // for DRS timer logic
+//  HAL_TIM_Base_Start(&htim2);  // for DRS timer logic
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+//	  if(!HAL_GPIO_ReadPin(BTN_REG, MANUAL_BTN))
+//	  		  mode = toggle((int)mode);
+//
+//	  	  if(mode == MANUAL)
+//	  	  {
+//	  		  // check if drs is toggled
+//	  		  if(!HAL_GPIO_ReadPin(BTN_REG, ON_BTN)){
+//	  			  drs_stat = toggle((int)drs_stat);
+//	  		  }
+//	  	  }
+//	  	  else
+//	  	  {
+//
+//	  	  }
     /* USER CODE END WHILE */
-	  if(!HAL_GPIO_ReadPin(BTN_REG, MANUAL_BTN))
-		  mode = toggle((int)mode);
 
-	  if(mode == MANUAL)
-	  {
-		  // check if drs is toggled
-		  if(!HAL_GPIO_ReadPin(BTN_REG, ON_BTN)){
-			  drs_stat = toggle((int)drs_stat);
-		  }
-	  }
-	  else
-	  {
-
-	  }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-}
-
-int toggle(int to_toggle)
-{
-	return (to_toggle + 1) % 2;
-}
-
-void enableCANRx()
-{
-
-}
-
-void disableCANRx()
-{
-
 }
 
 /**
@@ -210,6 +201,39 @@ static void MX_CAN_Init(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -230,18 +254,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
